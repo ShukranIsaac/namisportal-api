676fb70651d6785b5bc4b9fd025dae359cccfe08
Paul Sembereka <psemberekajr@gmail.com>
2019-02-05 19:46:23 +0200

    you can now query for a category by name in categories/service.js

--- src/components/categories/service.js
+++ src/components/categories/service.js
@@ -3,17 +3,17 @@ const Category = require('./model')
 module.exports = {
     getAll: async (name) => {
         if (name !== undefined){
-            return await Category.findOne({properties: {name}}).lean()
+            return await Category.findOne({name}).lean()
         }else{
             return await Category.find({}).lean()
         }
     },
-    getById: async (id) => await Category.findById(id).populate('subCategories').lean(),
+    getById: async (id) => await Category.findById(id).lean(),
     getDocuments: async (id) => await Category.findById(id).populate('documents').lean(),
     getSubCategories: async (id) => await Category.findById(id).populate('subCategories').lean(),
     getByIdMongooseUse: async (id) => await Category.findById(id),
     createOne: async (newCategory) => await Category.create(newCategory),
-    delete: async (id) => await Category.findByIdAndDelete(id)                                                                                                       
+    delete: async (id) => await Category.findByIdAndDelete(id).lean()                                                                                                       
 }
 
 
