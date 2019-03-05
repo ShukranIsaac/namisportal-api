632e6d385a2f51d25f9649da990982f6735282c0
Paul Sembereka <psemberekajr@gmail.com>
2019-03-05 15:37:06 +0200

    level 2 nesting with the subcategories

--- src/components/categories/service.js
+++ src/components/categories/service.js
@@ -1,14 +1,16 @@
 const Category = require('./model')
 
+const populate = { path: 'subCategories', populate: { path: 'subCategories' }}
+
 module.exports = {
     getAll: async (name) => {
         if (name !== undefined){
-            return await Category.findOne({name}).populate('subCategories').lean()
+            return await Category.findOne({name}).populate(populate).lean()
         }else{
             return await Category.find({}).lean()
         }
     },
-    getById: async (id) => await Category.findById(id).populate('subCategories').lean(),
+    getById: async (id) => await Category.findById(id).populate(populate).lean(),
     getDocuments: async (id) => await Category.findById(id).populate('documents').lean(),
     getSubCategories: async (id) => await Category.findById(id).populate('subCategories').lean(),
     getByIdMongooseUse: async (id) => await Category.findById(id),
