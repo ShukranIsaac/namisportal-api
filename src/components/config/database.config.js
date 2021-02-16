c03869e4f4e8b1b004077b0ce2544a367ffb7304
ShukranIsaac <imwakabira@ymail.com>
2021-02-16 15:33:46 +0200

    gitignore .env

--- src/components/config/database.config.js
+++ src/components/config/database.config.js
@@ -1,44 +1,54 @@
 const Sequelize = require('sequelize');
 const logger = require('morgan');
 
-// module.exports = new Sequelize(process.env.DATABASE_URL, {
-//     dialect: process.env.DATABASE_DIALECT,
-//     password: process.env.DATABASE_PASSWORD,
-//     user: process.env.DATABASE_USER,
-//     database: process.env.DATABASE_NAME,
-//     logging: logger.bind(this), //(...msg) => console.log(msg) or false
-//     pool: {
-//         max: 1,
-//         // acquire: 30000,
-//         // idle: 10000
-//     },
-//     // isolation level of each transaction
-//     // defaults to dialect default
-//     isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.REPEATABLE_READ
-// });
+const DATABASE_URL = 'postgres://namis@localhost:5432/namisportal'
 
-// psql -h ec2-34-194-198-176.compute-1.amazonaws.com -p 5432 -d dldjaab0u9g55 -U vdssmrfbnomigk -W -f namisportaldump.sql
+const DATABASE_USER = 'namis'
 
-module.exports = new Sequelize('postgres://vdssmrfbnomigk:198b00bd8259c15f4e7e2836a779535568dce85b5e189fce9cbd8e81568b3264@ec2-34-194-198-176.compute-1.amazonaws.com:5432/dldjaab0u9g55', {
-    dialect: "postgres",
-    password: "198b00bd8259c15f4e7e2836a779535568dce85b5e189fce9cbd8e81568b3264",
-    user: "vdssmrfbnomigk",
-    database: "dldjaab0u9g55",
-    host: "ec2-34-194-198-176.compute-1.amazonaws.com",
+const DATABASE_PASSWORD = 'namis@123'
+
+const DATABASE_DIALECT = 'postgres'
+
+const DATABASE_NAME = 'namisportal'
+
+module.exports = new Sequelize(DATABASE_URL, {
+    dialect: DATABASE_DIALECT,
+    password: DATABASE_PASSWORD,
+    user: DATABASE_USER,
+    database: DATABASE_NAME,
     logging: logger.bind(this), //(...msg) => console.log(msg) or false
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false // <<<<<<< YOU NEED THIS
-        }
-    },
     pool: {
         max: 10,
         // acquire: 30000,
-        // idle: 100,
-        // maxUses: Infinity
+        // idle: 10000
     },
     // isolation level of each transaction
     // defaults to dialect default
     isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.REPEATABLE_READ
 });
+
+// psql -h ec2-34-194-198-176.compute-1.amazonaws.com -p 5432 -d dldjaab0u9g55 -U vdssmrfbnomigk -W -f namisportaldump.sql
+
+// module.exports = new Sequelize('postgres://vdssmrfbnomigk:198b00bd8259c15f4e7e2836a779535568dce85b5e189fce9cbd8e81568b3264@ec2-34-194-198-176.compute-1.amazonaws.com:5432/dldjaab0u9g55', {
+//     dialect: "postgres",
+//     password: "198b00bd8259c15f4e7e2836a779535568dce85b5e189fce9cbd8e81568b3264",
+//     user: "vdssmrfbnomigk",
+//     database: "dldjaab0u9g55",
+//     host: "ec2-34-194-198-176.compute-1.amazonaws.com",
+//     logging: logger.bind(this), //(...msg) => console.log(msg) or false
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+//         }
+//     },
+//     pool: {
+//         max: 10,
+//         // acquire: 30000,
+//         // idle: 100,
+//         // maxUses: Infinity
+//     },
+//     // isolation level of each transaction
+//     // defaults to dialect default
+//     isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.REPEATABLE_READ
+// });
